fab40bb7a7d49d5b4c76fb2cc6caa65cd1fff228
Mylene88 <myleneyo00@yahoo.com>
2022-04-23 22:32:29 +0200

    premier commit

---
+++ CLI.py
@@ -0,0 +1,10 @@
+from argparse import ArgumentParser
+
+def parse_args() -> Namespace:
+    parser = ArgumentParser()
+    parser.add_argument(
+        "-c", "--config", help="Config file", required=True, dest="config"
+    )
+    return parser.parse_args()
+
+
